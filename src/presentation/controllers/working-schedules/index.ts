0ff79a6a7f823843e39b76106139d741bfe18d05
Erick Scur <erickscur@gmail.com>
2023-01-18 19:03:46 -0300

    fix: typo in file name

--- src/presentation/controllers/working-schedules/index.ts
+++ src/presentation/controllers/working-schedules/index.ts
@@ -1,4 +1,4 @@
 export * from './create-working-schedule.controller';
 export * from './get-restaurant-working-schedules.controller';
 export * from './update-working-schedule.controller';
-export * from './get-restaurant-formatted-working-schedulles.controller';
+export * from './get-restaurant-formatted-working-schedules.controller';
