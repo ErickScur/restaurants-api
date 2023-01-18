084bf04fa0fa50cebb0adfb5ecd1a4de7ca7a8cc
Erick Scur <erickscur@gmail.com>
2023-01-18 09:44:08 -0300

    feat: add swagger decorators to controller

--- src/presentation/controllers/restaurants/getAll/get-all-restaurants.controller.ts
+++ src/presentation/controllers/restaurants/getAll/get-all-restaurants.controller.ts
@@ -1,11 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GetAllRestaurants } from 'src/domain/use-cases/restaurants/get-all-restaurants';
 import { RestaurantResponseViewModel } from 'src/presentation/view-models/restaurants/restaurant-vm';
 
+@ApiTags('Restaurants')
 @Controller('restaurants')
 export class GetAllRestaurantsController {
   constructor(private readonly getAllRestaurants: GetAllRestaurants) {}
 
+  @ApiOperation({ summary: 'Get All Restaurants' })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    type: RestaurantResponseViewModel,
+    isArray: true,
+  })
+  @Get()
   async handle(): Promise<RestaurantResponseViewModel[]> {
     const restaurants = await this.getAllRestaurants.getAll();
 
