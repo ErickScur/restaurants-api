7bc3685b244d77913f72d06e191b8b1f6d23164c
Erick Scur <erickscur@gmail.com>
2023-01-17 21:49:30 -0300

    feat: add swagger document builder

---
+++ src/env.ts
@@ -0,0 +1,3 @@
+export const PORT: number = parseInt(process.env.PORT) || 3009;
+
+export const API_VERSION: string = process.env.API_VERSION || 'v1';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { API_VERSION, PORT } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix(`api/${API_VERSION}`);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Restaurants API')
+    .setDescription('Restaurants API')
+    .setVersion(API_VERSION)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(PORT);
 }
 bootstrap();
