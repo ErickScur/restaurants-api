9a0c330ca30e823a104b6f8ce10b6772616a990c
Erick Scur <erickscur@gmail.com>
2023-01-18 19:03:29 -0300

    refactor: add separate controller for formatted working schedules

--- src/presentation/controllers/working-schedules/get-restaurant-working-schedules.controller.ts
+++ src/presentation/controllers/working-schedules/get-restaurant-working-schedules.controller.ts
@@ -1,27 +1,27 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { GetFormattedWorkingSchedule } from 'src/domain/use-cases/working-schedules';
+import { GetRestaurantWorkingSchedules } from 'src/domain/use-cases/working-schedules';
 import { HttpBadRequestError } from 'src/presentation/swagger';
-import { GetRestaurantWorkingScheduleVM } from 'src/presentation/view-models/working-schedules/get-restaurant-working-schedule-vm';
+import { WorkingScheduleVM } from 'src/presentation/view-models/working-schedules/working-schedule-vm';
 
 @ApiTags('Working Schedules')
 @Controller('restaurants')
 export class GetRestaurantWorkingScheduleController {
   constructor(
-    private readonly getRestaurantWorkingSchedule: GetFormattedWorkingSchedule,
+    private readonly getRestaurantWorkingSchedule: GetRestaurantWorkingSchedules,
   ) {}
 
   @ApiOperation({ summary: 'Get Restaurant Working Schedule' })
   @ApiResponse({
     status: 200,
     description: 'Success',
-    type: GetRestaurantWorkingScheduleVM,
+    type: WorkingScheduleVM,
     isArray: true,
   })
   @ApiResponse(HttpBadRequestError)
   @Get(':restaurantId/working-schedules')
   async handle(@Param('restaurantId') restaurantId: string) {
-    return await this.getRestaurantWorkingSchedule.getWorkingSchedule(
+    return await this.getRestaurantWorkingSchedule.getWorkingSchedules(
       restaurantId,
     );
   }
